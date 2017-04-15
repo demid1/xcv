d1c6c4c44cbf0e70e175258c7bbbd92405c419c9
Demid <sorokin.demid1@yandex.ry>
2017-04-15 21:37:37 +0500

    first commit

---
+++ check.php
@@ -0,0 +1,65 @@
+﻿ <?php
+	$num1=$_POST['number1'];
+	$num2=$_POST['number2'];
+	$plus=$_POST['plus'];
+	$minus=$_POST['minus'];
+	$multiplication=$_POST['multiplication'];
+	$deference=$_POST['deference'];
+	$do=$_POST['do'];
+	if(isset($num1) && isset($num2))
+	{
+		if($plus=='on')
+		{
+			  $res=$num1+$num2;
+		}
+		if($minus=='on')
+		{
+			 $res=$num1-$num2;
+		}
+		if($multiplication=='on')
+		{
+			 $res=$num1*$num2;
+		}
+
+		if($deference=='on')
+		{
+			 $res=$num1/$num2;
+		}
+
+	}
+			else
+		{
+			echo "введите числа";
+		}
+	
+	
+ ?>
+<!DOCTYPE html>
+<html>
+ <head>
+	<title>калькулятор</title>
+ </head>
+<body>
+ <center><font color="red" size="5">калькулятор</font></center>
+ <br><br><br>
+ <center><table border="2"  >
+ <form action="index.php" method="post">
+ <tr></tr>
+ <td colspan="4"><center>введите число:<input type="number"  name="number3"></center></td>
+ <tr></tr>
+ <td width="25%" ><center>+<input type="radio" name="plusi"></center></td>
+ <td width="25%"><center>-<input type="radio" name="minusi"></center></td>
+ <td width="25%"><center>*<input type="radio" name="multiplicationi"></center></td>
+ <td width="25%"><center>/<input type="radio" name="deferencei"></center></td>
+ <tr></tr>
+ <td colspan="4"><center>введите число:<input type="number"  name="number4"></center></td>
+  <tr></tr>
+ <td colspan="4"><center>результат:<?php echo $res; ?></center></td>
+ <tr></tr>
+ <td colspan="2"><center><input type="submit" name="doi" value="вычислить"></center></td>
+ <td colspan="2"><center><input type="reset" name="ci" value="сбросить"></center></td>
+ 
+
+ </table></center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+﻿ <?php
+	$num3=$_POST['number3'];
+	$num4=$_POST['number4'];
+	$plusi=$_POST['plusi'];
+	$minusi=$_POST['minusi'];
+	$multiplicationi=$_POST['multiplicationi'];
+	$deferencei=$_POST['deferencei'];
+	
+	if(isset($num3) && isset($num4))
+	{
+		if($plusi=='on')
+		{
+			  $resi=$num3+$num4;
+		}
+		if($minusi=='on')
+		{
+			 $resi=$num3-$num4;
+		}
+		if($multiplicationi=='on')
+		{
+			 $resi=$num3*$num4;
+		}
+
+		if($deferencei=='on')
+		{
+			 $resi=$num3/$num4;
+		}
+
+	}
+			else
+		{
+			echo "введите числа";
+		}
+	
+ ?>
+<!DOCTYPE html>
+<html>
+ <head>
+	<title>калькулятор</title>
+ </head>
+<body >
+ <center><font color="red" size="5">калькулятор</font></center>
+ <br><br><br>
+ <center><table border="2"  >
+ <form action="check.php" method="post">
+ <tr></tr>
+ <td colspan="4"><center>введите число:<input type="number"  name="number1"></center></td>
+ <tr></tr>
+ <td width="25%" ><center>+<input type="radio" name="plus"></center></td>
+ <td width="25%"><center>-<input type="radio" name="minus"></center></td>
+ <td width="25%"><center>*<input type="radio" name="multiplication"></center></td>
+ <td width="25%"><center>/<input type="radio" name="deference"></center></td>
+ <tr></tr>
+ <td colspan="4"><center>введите число:<input type="number"  name="number2"></center></td>
+ <tr></tr>
+ <td colspan="4"><center>результат:<? echo $resi ?></center></td>
+ <tr></tr>
+ <td colspan="2"><center><input type="submit" name="do" value="вычислить"></center></td>
+ <td colspan="2"><center><input type="reset" name="c" value="сбросить"></center></td>
+ </table></center>
+</body>
+</html>
